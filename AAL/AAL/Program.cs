e4ee1fe78df30e3fc85ba5da3e1bbb953f622724
shooboi <82247284+shooboi@users.noreply.github.com>
2022-07-12 21:06:30 +0700

    update admin area

--- AAL/AAL/Program.cs
+++ AAL/AAL/Program.cs
@@ -35,10 +35,20 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+app.MapRazorPages();
 
-app.MapControllerRoute(
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapAreaControllerRoute(
+       name: "areas",
+       areaName: "Admin",
+       pattern: "Admin/{controller=Home}/{action=Index}/{id?}"
+   );
+
+    endpoints.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+});
+
 
 app.Run();
