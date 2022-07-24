92333c5bb25164377e4c3e771e2d32d736c81540
DonnieNguyen <anhhuy140301@gmail.com>
2022-07-24 10:46:48 +0700

    Show Data In HomePage

---
+++ AAL/AAL/Controllers/ItemsController.cs
@@ -0,0 +1,45 @@
+﻿using AAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AAL.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly AALContext _context;
+
+        public ItemsController(AALContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(Category? category)
+        {
+            ViewData["PageTitle"] = "All Products";
+            //if (category.CatName)
+            //{
+               
+            //} 
+            var ItemList = _context.Items .ToList();
+            return View(ItemList);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Items == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .Include(i => i.Cat)
+                .Include(i => i.Warehouse)
+                .FirstOrDefaultAsync(m => m.ItemId == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+    }
+}
